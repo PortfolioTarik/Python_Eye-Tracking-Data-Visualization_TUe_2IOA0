e66834421be56fa5436ea6eea3dedd78b12bb183
TarikHaci <tarikhaci@outlook.com>
2020-04-26 15:46:44 +0200

    Established a linchart with Bokeh and enabled to Read the CSV and show it in this line chart
    2 tasks

--- visualization_linechart/views.py
+++ visualization_linechart/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
+from bokeh.plotting import figure, output_file, show
+from bokeh.embed import components
+import pandas as pd
 
 
 def home(request):
-    context = {
-        'var': 10
-    }
-    return render(request, 'website_linechart.html', context)
+    df_eye = pd.read_csv(
+        'C:/Users/TARIKSSD/Desktop/2IOA0 - Project/2ioa0-23a-eye-tracking-and-visualization/import_csv/csv/all_fixation_data_cleaned_up.csv', encoding='unicode_escape', sep="\t")
+    #source = ColumnDataSource(df_eye)
+    title = 'y = f(x)'
+
+    plot = figure(title=title,
+                  x_axis_label='X-Axis',
+                  y_axis_label='Y-Axis',
+                  plot_width=400,
+                  plot_height=400)
+
+    plot.line(df_eye['Timestamp'], df_eye['MappedFixationPointX'],
+              legend_label='f(x)', line_width=2)
+    # Store components
+    script, div = components(plot)
+
+    # Feed them to the Django template.
+    return render(request, 'website_import.html',
+                  {'script': script, 'div': div})
