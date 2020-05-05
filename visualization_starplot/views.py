0d8a54b8385cc17aa1db9f9298a80c220eee471a
TarikHaci <tarikhaci@outlook.com>
2020-05-05 14:14:53 +0200

    Gazeplot: Displaying numerical label on top of the nodes (circles) + made it OOP (for Modal) and Optimalized efficiency by sets
    Huge Update!

--- visualization_starplot/views.py
+++ visualization_starplot/views.py
@@ -1,3 +1,4 @@
+# Coded by Tarik Hacialiogullari unless when stated
 from django.shortcuts import render
 
 # Create your views here.
@@ -11,77 +12,52 @@ from bokeh.plotting import figure, show, output_file
 import os
 
 
+def addUserToGraph(userDataOriginal, p, color):
+    userData = userDataOriginal.copy()
+    user = userData['user'].iloc[0]
+    sizePath = len(userData.index)
+    x_coordinates = userData["MappedFixationPointX"]
+    y_coordinates = userData["MappedFixationPointY"]
+    texts = list(range(1, sizePath + 1))
+    sizes = [userData["FixationDuration"].iloc[[i]] /
+             15 for i in list(range(0, sizePath))]
+    source = ColumnDataSource(data=dict(x_coordinates=x_coordinates,
+                                        y_coordinates=y_coordinates,
+                                        texts=texts,
+                                        sizes=sizes))
+    # line
+    p.line(x_coordinates, y_coordinates,
+           legend_label=user, line_width=2, color=color)
+    # nodes
+    p.circle('x_coordinates', 'y_coordinates', fill_color="black",
+             color=color, size='sizes', line_width=3, source=source)
+    # texts
+    p.add_layout(LabelSet(x='x_coordinates', y='y_coordinates', text='texts', level='overlay',
+                          x_offset=5, y_offset=5, text_color=color, source=source, render_mode='canvas'))
+    return
+
+
 def home(request):
     workpath = os.path.dirname(os.path.abspath(__file__))
     df = pd.read_csv(workpath +
                      '/all_fixation_data_cleaned_up.csv', encoding='unicode_escape', sep="\t")
-
-    # copy dataset with specialized columns (just for testing something)
+    # copy dataset with specialized columns (By Fanni)
     test = df[['user', 'StimuliName']].copy()
-    # print(test[(test['StimuliName'] == '07_Moskau_S1.jpg') & (test['user'] == 'p1')])
-
-    # print(test[test['user'] == "p2"] & test[test['StimuliName'] == "07_Moskau_S1.jpg"])
-    # prepare some data 3 criteria
     userData = df[(test['StimuliName'] == '06_Hamburg_S1.jpg')
                   & (test['user'] == 'p1')]
-    x = df[(test['StimuliName'] == '06_Hamburg_S1.jpg') &
-           (test['user'] == 'p1')]["MappedFixationPointX"]
-    y = df[(test['StimuliName'] == '06_Hamburg_S1.jpg') &
-           (test['user'] == 'p1')]["MappedFixationPointY"]
-    z = df[(test['StimuliName'] == '06b_Hamburg_S2.jpg') &
-           (test['user'] == 'p16')]["MappedFixationPointX"]
-    w = df[(test['StimuliName'] == '06b_Hamburg_S2.jpg') &
-           (test['user'] == 'p16')]["MappedFixationPointY"]
-
-    # output to static HTML file
-    # output_file("lines.html")
-
-    # create a new plot with a title and axis labels
+    userData2 = df[(test['StimuliName'] == '06_Hamburg_S1.jpg')
+                   & (test['user'] == 'p16')]
+    # create a new plot with a title and axis labels (by Fanni)
     p = figure(plot_width=800, plot_height=600, x_range=(0, 1651), y_range=(0, 1200),
-               title="Gaze plot of Hamburg of the first user", x_axis_label='Mapped Fixation Point X',
+               title="Gaze plot of Hamburg of users", x_axis_label='Mapped Fixation Point X',
                y_axis_label='Mapped Fixation Point Y')
-
+    # background
     p.image_url(url=[
         'https://i.ibb.co/VQSkMnN/06-Hamburg-S1.jpg'], x=0, y=1200, w=1651, h=1200)
 
-    # add a line renderer with legend and line thickness
-    p.line(x, y, legend_label="P1.", line_width=2, color="red")
-    p.line(z, w, legend_label="P16.", line_width=2, color="blue")
-    # count = 0
-#     for hor in x:
-#         ver = y.iloc[count]
-#         count = count + 1
-#         print('x= ' + str(hor) + ' ver= ' + str(ver))
-#         p.circle(hor, ver, fill_color="red",
-#                  color="red", size=8)
-    sizePath = x.count()
-    numericalArr = list(range(1, sizePath + 1))
-    fixDur = userData['FixationDuration']
-    fixationDurationArr = [fixDur.iloc[[i]] /
-                           15 for i in list(range(0, sizePath))]
-    # numericalArr = [str(i) for i in list(range(1, sizePath))]
-
-    source = ColumnDataSource(data=dict(height=y,
-                                        weight=x,
-                                        names=numericalArr,
-                                        fixationDuration=fixationDurationArr))
-#     count = 0
-#     for index, row in userData.iterrows():
-    # p.circle(row['MappedFixationPointX'], row['MappedFixationPointY'],
-    #         fill_color="black", color="red", size=row['FixationDuration']/15, name='len')
-    #  p.add_layout(Label(x=row['MappedFixationPointX'], y=row['MappedFixationPointY'], text=str(count), level='glyph',
-    #                     x_offset=5, y_offset=5, render_mode='canvas'))
-    #  count = count + 1
-    p.circle('weight', 'height', fill_color="black",
-             color="red", size='fixationDuration', source=source)
-    # p.circle(x, y, fill_color="red",
-    # color="red", size=8)
-
-    # x.head()
-    p.add_layout(LabelSet(x='weight', y='height', text='names', level='overlay',
-                          x_offset=5, y_offset=5, text_color="red", source=source, render_mode='canvas'))
-    # add background
-
+    # adding users to the graoh
+    addUserToGraph(userData, p, 'red')
+    addUserToGraph(userData2, p, 'yellow')
     # show the results
     script, div = components(p)
     return render(request, 'website_starplot.html',
