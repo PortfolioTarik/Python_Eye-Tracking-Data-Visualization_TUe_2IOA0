d7b80e9e23e7483afae8c4730c8ec22179a5434e
lnvanderBij <64069819+lnvanderBij@users.noreply.github.com>
2020-05-06 15:58:16 +0200

    Worked on uploading datasets

--- import_csv/views.py
+++ import_csv/views.py
@@ -8,18 +8,24 @@ from import_csv.models import FixationData
 
 def upload_csv(request):
     template = "website_import.html"
-    data = FixationData.objects.all()
-    
-    prompt = {'order': 'test',
-    'profiles': data}
+    data = FixationData.objects.all()        
+
+    prompt = {'profiles': data}
 
     if request.method == "GET":
         return render(request,template,prompt)
 
     csvfile = request.FILES['file']
     
-    if not csvfile.name.endswith('csv'):
-        messages.error(request, 'This is not a csv file')
+    if not csvfile.name.endswith('.csv'):
+        list(messages.get_messages(request))
+        messages.add_message(request, messages.INFO, 'This is not a csv file')
+        return render(request,template,prompt)
+    
+    if csvfile.name.endswith('csv'):
+        list(messages.get_messages(request))
+        messages.add_message(request, messages.INFO, 'csv file succesfully uploaded')
+
 
     dataset = csvfile.read().decode('UTF-8', 'replace')
 
