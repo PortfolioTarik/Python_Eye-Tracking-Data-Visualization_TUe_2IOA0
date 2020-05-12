0c7d332b65e0c90b902e2054f4ddae599bfa0478
TarikHaci <tarikhaci@outlook.com>
2020-05-12 09:37:01 +0200

    Back-up (not working solution)

--- import_csv/models.py
+++ import_csv/models.py
@@ -2,16 +2,18 @@ from django.db import models
 import os
 from django.conf import settings
 
+
 def stimuli_path():
     return os.path.join(settings.BASE_DIR, 'stimuli/')
 
+
 class FixationData(models.Model):
-    timestamp = models.IntegerField()
-    stimuli_name = models.FilePathField(path=stimuli_path)
-    fixation_index = models.IntegerField()
-    fixation_duration = models.IntegerField()
-    mapped_fixation_point_X = models.IntegerField()
-    mapped_fixation_point_Y = models.IntegerField()
+    Timestamp = models.IntegerField()
+    StimuliName = models.FilePathField(path=stimuli_path)
+    FixationIndex = models.IntegerField()
+    FixationDuration = models.IntegerField()
+    MappedFixationPointX = models.IntegerField()
+    MappedFixationPointY = models.IntegerField()
     user = models.CharField(max_length=5)
     description = models.CharField(max_length=100)
 
@@ -20,4 +22,5 @@ class FixationData(models.Model):
         db_table = 'Fixation_data'
 
     def __str__(self):
-        return self.stimuli_name
+        return self.StimuliName
+        # return self.Timestamp + ' ' + self.MappedFixationPointX + ' ' + self.MappedFixationPointY
