2586e2ed4e972b32906440bde6021ad704b67417
omar.salem02 <omar.ksalem02@gmail.com>
2020-06-09 21:23:05 +0200

    Same as previous

--- visualization_linechart/views.py
+++ visualization_linechart/views.py
@@ -1,4 +1,4 @@
-# Generated and coded by Omar Salem except when noted.(Few changes by Tarik Haci)
+# Generated and coded by Omar Salem except when noted.
 
 from django.shortcuts import render
 
@@ -13,17 +13,20 @@ from bokeh.layouts import row
 from homepage.models import getUserData
 
 def addUserToGraph(userData, p, color):
+#--- Select Timestamp and Fixation duration on graph 
     x = userData['Timestamp']
     y = userData['FixationDuration']
 #---Start Coding by Tarik Haci
     user = userData['user'].iloc[0]
     #---End Coding by Tarik Haci
+    #Creating Line and dots
     p.line(x, y, legend_label=user, line_width=2, color=color)
     p.circle(x, y, fill_color="black",
               color=color, size=6)
 
     return
 
+#---Putting Toolbar and creatig Linechart
 def getGraph(toolbar):
     p = figure(plot_width=600, plot_height=450, y_range=(100, 700),
                 title="Line Chart of Fixation Duration per Timestamp", x_axis_label='Timestamp',
@@ -37,7 +40,8 @@ def home(request):
     addUserToGraph(getUserData('p1', '06_Hamburg_S1.jpg', 'color', 'red'), p)
     addUserToGraph(getUserData('p16', '06_Hamburg_S1.jpg', 'color', 'yellow'), p, 'yellow')
     addUserToGraph(getUserData('p12', '06_Hamburg_S1.jpg', 'color', 'blue'), p, 'blue')
-#---End Coding by Tarik Haci
+
     script, div = components(row(p))
     return render(request, 'website_starplot.html',
                   {'script': script, 'div': div})
+#--- End Coding by Tarik Haci
