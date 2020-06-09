75bc2922863ae61ebc6b66cc88a03168e9fa7860
omar.salem02 <omar.ksalem02@gmail.com>
2020-06-09 21:20:18 +0200

    Linechart changes to range and comments

--- visualization_linechart/views.py
+++ visualization_linechart/views.py
@@ -1,4 +1,4 @@
-# Generated and coded by Tarik Hacialiogullari except when noted.
+# Generated and coded by Omar Salem except when noted.(Few changes by Tarik Haci)
 
 from django.shortcuts import render
 
@@ -12,34 +12,32 @@ from bokeh.plotting import figure, show, output_file
 from bokeh.layouts import row
 from homepage.models import getUserData
 
-
 def addUserToGraph(userData, p, color):
     x = userData['Timestamp']
     y = userData['FixationDuration']
+#---Start Coding by Tarik Haci
     user = userData['user'].iloc[0]
-    #---Start Coding by Omar Salim & Updated by Tarik Hacialiogullari
+    #---End Coding by Tarik Haci
     p.line(x, y, legend_label=user, line_width=2, color=color)
     p.circle(x, y, fill_color="black",
               color=color, size=6)
-    #---End Coding by Omar Salim & Updated by Tarik Hacialiogullari
 
     return
 
 def getGraph(toolbar):
-    #---Start Coding by Omar Salim & Updated by Tarik Hacialiogullari
-    p = figure(plot_width=600, plot_height=450, x_range=(38000, 43000), y_range=(100, 700),
+    p = figure(plot_width=600, plot_height=450, y_range=(100, 700),
                 title="Line Chart of Fixation Duration per Timestamp", x_axis_label='Timestamp',
                 y_axis_label='FixationDuration', tools=toolbar)
-    #---End Coding by Omar Salim & Updated by Tarik Hacialiogullari
     return p
 
+#---Start Coding by Tarik Haci
 def home(request):
     toolbar = "box_select, lasso_select, wheel_zoom, pan, reset, save, hover, help"
     p = getGraph(toolbar)
     addUserToGraph(getUserData('p1', '06_Hamburg_S1.jpg', 'color', 'red'), p)
     addUserToGraph(getUserData('p16', '06_Hamburg_S1.jpg', 'color', 'yellow'), p, 'yellow')
     addUserToGraph(getUserData('p12', '06_Hamburg_S1.jpg', 'color', 'blue'), p, 'blue')
-
+#---End Coding by Tarik Haci
     script, div = components(row(p))
     return render(request, 'website_starplot.html',
                   {'script': script, 'div': div})
