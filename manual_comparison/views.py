03cf60ca05e1f4cc32d520804df546b201ba051c
fanniegresits <64306039+fanniegresits@users.noreply.github.com>
2020-06-10 12:17:27 +0200

    First dtry of manual comparison dropdown

--- manual_comparison/views.py
+++ manual_comparison/views.py
@@ -19,100 +19,151 @@ from visualization_barchart.views import addUserToGraph as addUserToGraphBar
 from visualization_linechart.views import getGraph as getGraphLine
 from visualization_linechart.views import addUserToGraph as addUserToGraphLine
 
-#Some of them are for background not sure which one, just putting all in it m8.
+# Some of them are for background not sure which one, just putting all in it m8.
 import eye_tracking_visualizations_group23a.settings
 from PIL import Image
 import requests
 from io import BytesIO
 from django.contrib.staticfiles.storage import staticfiles_storage
 
-#SEE homepage/views.py for the comments
+
+# SEE homepage/views.py for the comments
 def home(request):
     toolbar = "box_select, lasso_select, wheel_zoom, pan, reset, save, hover, help"
 
+    # stimuli has to be the same for left and right
     stimuli = '06_Hamburg_S1.jpg'
-    user = 'p1'
-    color = 'color'
-    brev = False
-    graph = 'gaze'
+    user_left = 'p1'
+    color_left = 'color'
+    brev_left = False
+    graph_left = 'gaze'
+
+    user_right = 'p1'
+    color_right = 'color'
+    brev_right = False
+    graph_right = 'gaze'
 
     if request.GET.get('stimuli') is not None:
         stimuli = request.GET['stimuli']
         print('STIMULI IS RECEIVED:' + stimuli)
 
-    if request.GET.get('user') is not None:
-        user = request.GET['user']
-        print('USER IS RECEIVED:' + user)
+    if request.GET.get('user_left') is not None:
+        user_left = request.GET['user_left']
+        print('USER IS RECEIVED:' + user_left)
+    if request.GET.get('user_right') is not None:
+        user_right = request.GET['user_right']
+        print('USER IS RECEIVED:' + user_right)
 
-    if request.GET.get('color') is not None:
-        color = request.GET['color']
-        print('COLOR IS RECEIVED:' + color)
-    
-    #same as stimuli but for barchart so that you can reverse it order.
-    if request.GET.get('brev') is not None:
-        brev = parseToBool(request.GET['brev'].lower())
-        print('BREV IS RECEIVED:' + str(brev))
+    if request.GET.get('color_left') is not None:
+        color_left = request.GET['color_left']
+        print('COLOR IS RECEIVED:' + color_left)
+    if request.GET.get('color_right') is not None:
+        color_right = request.GET['color_right']
+        print('COLOR IS RECEIVED:' + color_right)
+
+    # same as stimuli but for barchart so that you can reverse it order.
+    if request.GET.get('brev_left') is not None:
+        brev_left = parseToBool(request.GET['brev_left'].lower())
+        print('BREV IS RECEIVED:' + str(brev_left))
+    if request.GET.get('brev_right') is not None:
+        brev_right = parseToBool(request.GET['brev_right'].lower())
+        print('BREV IS RECEIVED:' + str(brev_right))
 
     # Coded by Laura
-    if request.GET.get('graph') is not None:
-        graph = request.GET['graph']
-        print('GRAPH IS RECEIVED' + graph)
+    if request.GET.get('graph_left') is not None:
+        graph_left = request.GET['graph_left']
+        print('GRAPH IS RECEIVED' + graph_left)
+    if request.GET.get('graph_right') is not None:
+        graph_right = request.GET['graph_right']
+        print('GRAPH IS RECEIVED' + graph_right)
         # end coding Laura
 
-    #getData
-    df_userOne = getUserData(user, stimuli, color)
+    # getData
+    df_userLeft = getUserData(user_left, stimuli, color_left)
+    df_userRight = getUserData(user_right, stimuli, color_right)
 
     # ---Start Coding by Fanni Egresits
-    #GetBackground images
+    # GetBackground images
     url = '/static/stimuli/{}'.format(stimuli)
     img_url = "http://" + request.get_host() + url
 
-        #Get the parameters of map (width, height)
+    # Get the parameters of map (width, height)
     response = requests.get(img_url)
     w, h = Image.open(BytesIO(response.content)).size
     # ---End Coding by Fanni Egresits
 
-    selected_graph = 0
-
     # Coded by Fanni and Laura
-    if graph == 'gaze':
-        selected_graph = getGraphGaze(toolbar, url, w, h)
-        addUserToGraphGaze(df_userOne, selected_graph, 'red')
-        script_graph, selected_graph = components(selected_graph)
-    elif graph == 'line':
-        selected_graph = getGraphLine(toolbar)
-        addUserToGraphLine(df_userOne, selected_graph, 'red')
-        script_graph, selected_graph = components(selected_graph)
-    elif graph == 'bar':
-        end = len(df_userOne.index) + 1000
-        selected_graph = getGraphBar(toolbar, end)
-        addUserToGraphBar(df_userOne, selected_graph, 'red', 0, brev)
-        script_graph, selected_graph = components(selected_graph)
-    elif graph == 'contour':
-        selected_graph = getGraphContour(df_userOne, url, w, h)
-        script_graph = 0
-        #end coding Fanni and Laura
+    selected_graph_left = 0
+    script_graph_left = 0
+    if graph_left == 'gaze':
+        selected_graph_left = getGraphGaze(toolbar, url, w, h)
+        addUserToGraphGaze(df_userLeft, selected_graph_left, 'red')
+        script_graph_left , selected_graph_left = components(selected_graph_left)
+    elif graph_left == 'line':
+        selected_graph_left = getGraphLine(toolbar)
+        addUserToGraphLine(df_userLeft, selected_graph_left, 'red')
+        script_graph_left , selected_graph_left = components(selected_graph_left)
+    elif graph_left == 'bar':
+        end = len(df_userLeft.index) + 1000
+        selected_graph_left = getGraphBar(toolbar, end)
+        addUserToGraphBar(df_userLeft, selected_graph_left, 'red', 0, brev_left)
+        script_graph_left , selected_graph_left = components(selected_graph_left)
+    elif graph_left == 'contour':
+        selected_graph_left = getGraphContour(df_userLeft, url, w, h)
 
-    #Stimuli dropdown
+    selected_graph_right = 0
+    script_graph_right = 0
+    if graph_right == 'gaze':
+        selected_graph_right = getGraphGaze(toolbar, url, w, h)
+        addUserToGraphGaze(df_userRight, selected_graph_right, 'red')
+        script_graph_right , selected_graph_right = components(selected_graph_right)
+    elif graph_right == 'line':
+        selected_graph_right = getGraphLine(toolbar)
+        addUserToGraphLine(df_userRight, selected_graph_right, 'red')
+        script_graph_right , selected_graph_right = components(selected_graph_right)
+    elif graph_right == 'bar':
+        end = len(df_userRight.index) + 1000
+        selected_graph_right = getGraphBar(toolbar, end)
+        addUserToGraphBar(df_userRight, selected_graph_right, 'red', 0, brev_right)
+        script_graph_right , selected_graph_right = components(selected_graph_right)
+    elif graph_right == 'contour':
+        selected_graph_right = getGraphContour(df_userRight, url, w, h)
+
+        # end coding Fanni and Laura
+
+    # Stimuli dropdown
     stimuli_list = getAllStimulis()
     user_list = getAllUsersByStimuli(stimuli)
     graph_list = {'gaze', 'contour', 'bar', 'line'}
 
-    #Updated from homepage context by Fanni and Laura
+
+    #Convert to HTML
+    #script_graph, graph_toCompare = components(gridplot([selected_graph_left, selected_graph_right], ncols=2, sizing_mode="scale_both"))
+   # script_bar, graph_bar = components(graph_bar)
+
+    # Updated from homepage context by Fanni and Laura
     context = {
-        'graph_selected' : selected_graph,
-        'graphselection' : graph,
+        'selected_graph_left': selected_graph_left,
+        'selected_graph_right': selected_graph_right,
+        'graph_left': graph_left,
+        'graph_right': graph_right,
         'selected_stimuli': stimuli,
-        'selected_user': user,
-        'selected_color': color,
+        'selected_user_left': user_left,
+        'selected_user_right': user_right,
+        'selected_color_left': color_left,
+        'selected_color_right': color_right,
         'stimuli_list': stimuli_list,
         'user_list': user_list,
-        'script_graph': script_graph,
-        'graph_list' : graph_list,
+        'script_graph_left': script_graph_left,
+        'script_graph_right': script_graph_right,
+        'graph_list': graph_list
+
     }
     return render(request, 'manual.html', context)
+#'graph_toCompare': graph_toCompare,
 
-#Function from https://stackoverflow.com/a/53287252/4641129 to convert text content to boolean type. (only changed the function name)
+# Function from https://stackoverflow.com/a/53287252/4641129 to convert text content to boolean type. (only changed the function name)
 def parseToBool(string):
     d = {'true': True, 'false': False}
     return d.get(string, string)
+
